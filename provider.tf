d2a7c4f41e0ca352f31adf436a828739cf4537d6
yashs5211 <103175543+yashs5211@users.noreply.github.com>
2024-11-06 17:51:41 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,37 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
+parameters:
+  - name: projectName
+    displayName: "Enter project name"
+    type: string
+    default: "SampleProject"
+  - name: environment
+    displayName: "Select environment"
+    type: string
+    default: "dev"
+  - name: version
+    displayName: "Specify version"
+    type: string
+    default: "1.0.0"
 
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
+jobs:
+  - job: ReplacePlaceholders
+    steps:
+      - task: PowerShell@2
+        inputs:
+          targetType: 'inline'
+          script: |
+            # Read the template content
+            $templatePath = "$(Build.SourcesDirectory)\template.json"
+            $jsonContent = Get-Content -Path $templatePath -Raw
+
+            # Replace placeholders with runtime parameter values
+            $jsonContent = $jsonContent -replace '\$\{projectName\}', "$(projectName)"
+            $jsonContent = $jsonContent -replace '\$\{environment\}', "$(environment)"
+            $jsonContent = $jsonContent -replace '\$\{version\}', "$(version)"
+
+            # Output the updated JSON file
+            $outputPath = "$(Build.ArtifactStagingDirectory)\output.json"
+            $jsonContent | Out-File -FilePath $outputPath -Encoding utf8
+          
+      - publish: $(Build.ArtifactStagingDirectory)/output.json
+        artifact: updatedJson
+
